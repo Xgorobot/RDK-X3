1ecbb9f73a271f5b979990ee2ee68b7aec9887d4
YU MU <1973036444@qq.com>
2026-01-18 14:05:31 +0800

    serial

---
+++ serial/serial/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sunrise/serial/serial
+# Build directory: /home/sunrise/serial/serial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
